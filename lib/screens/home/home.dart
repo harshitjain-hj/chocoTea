cfaaad3cb4a9dd93ad2de78881dd467e463c7e00
Harshit <harshitjainf3@gmail.com>
2019-11-19 18:37:56 +0530

    Added data update widget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,6 +11,16 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context){
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: Text('bottom sheet'),
+          );
+      });
+    }
+
     return StreamProvider<List<Tea>>.value(
       value: DatabaseService().teas,
         child: Scaffold(
@@ -25,6 +35,11 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('settings'),
+              onPressed: () => _showSettingsPanel(),
             )
           ],
         ),
